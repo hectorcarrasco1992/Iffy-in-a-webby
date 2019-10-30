196ad597afba5cf8f3d062dc3d85b451c6275048
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-10-29 20:58:18 -0400

    removed comments

--- superCar.js
+++ superCar.js
@@ -1,6 +1,3 @@
-// depending on the ratio determine if car is a supercar  or not 
-// if less than 1 give back "Your car is not that special"
-// if  1 and above give back "You own a SuperCar"
 
 
 
@@ -11,18 +8,13 @@ function horsePowerToWeight(horsePower,weight){
 }
 
 
-// const hp = process.argv[2]
-// const weight = process.argv[3] 
-// const answer = horsePowerToWeight(hp, weight)
 
-// "But is it fast" if mph is equal or greater than 190, return "oh you're fast fast"
-// Otherwise, return "join the club bucko"
 
 function howFast(MPH){
     return MPH >= 190 ? "Oh you're FAST FAST": "Join the club Bucko"
 }
 
-// const speed = process.argv[4]
+
 
 let message = prompt('Please enter your car horse power and weight and speed');
 
